54c004da6b914f4e3a55e4582c435e3f513bcb72
fanning <fanning9386@gmail.com>
2022-03-31 16:09:01 +0900

    git init
    nuxt 디렉토리 
    레이아웃 (hero)
    nuxt.config.js - 태그추가 (css,icon)

---
+++ mycloud2/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <section class="hero is-primary">
+          <div class="hero-body">
+            <p class="title">
+              <i class="fas fa-dog"></i> 강아지와  <i class="fas fa-cat"></i>고양이의 동행
+            </p>
+            <p class="subtitle">
+                강아지와 고양이에 대한 모든 정보를 볼 수 있는 곳
+                안녕하세요
+            </p>
+        </div>
+        </section>
+    </div>
+</template>
