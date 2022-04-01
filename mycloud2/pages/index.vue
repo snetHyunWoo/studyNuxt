7f05f6f6c9c5c164d51f0b372795d67cb702c3da
fanning <fanning9386@gmail.com>
2022-04-01 09:32:44 +0900

    image tag

--- mycloud2/pages/index.vue
+++ mycloud2/pages/index.vue
@@ -11,5 +11,35 @@
             </p>
         </div>
         </section>
+        <div class="columns">
+          <div class="column">
+            <article class="message">
+              <div class="message-header">
+                <p>강아지</p>
+                <button class="delete" aria-label="delete"></button>
+              </div>
+              <div class="message-body">
+                <figure class="image is-128x128">
+                  <img src="https://interbalance.org/wp-content/uploads/2021/08/flouffy-VBkIK3qj3QE-unsplash-scaled-e1631077364762.jpg">
+                </figure>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </div>
+          <div class="column">
+            <article class="message">
+              <div class="message-header">
+                <p>고양이</p>
+                <button class="delete" aria-label="delete"></button>
+              </div>
+              <div class="message-body">
+                <figure class="image is-128x128">
+                  <img src="~assets/cat.jpg">
+                </figure>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </div>
+        </div>
     </div>
 </template>
