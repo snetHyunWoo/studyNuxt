5bf7feff27f0155342c72282b866a6a81c3bbe87
fanning <fanning9386@gmail.com>
2022-04-01 11:40:44 +0900

    table 생성, nuxt link-to

--- mycloud2/nuxt.config.js
+++ mycloud2/nuxt.config.js
@@ -59,6 +59,6 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {   
+  build: {
   }
 }
--- mycloud2/pages/index.vue
+++ mycloud2/pages/index.vue
@@ -1,45 +1,113 @@
 <template>
-    <div>
-        <section class="hero is-primary">
-          <div class="hero-body">
-            <p class="title">
-              <i class="fas fa-dog"></i> 강아지와  <i class="fas fa-cat"></i>고양이의 동행
-            </p>
-            <p class="subtitle">
-                강아지와 고양이에 대한 모든 정보를 볼 수 있는 곳
-                안녕하세요
-            </p>
-        </div>
-        </section>
-        <div class="columns">
-          <div class="column">
-            <article class="message">
-              <div class="message-header">
-                <p>강아지</p>
-                <button class="delete" aria-label="delete"></button>
-              </div>
-              <div class="message-body">
-                <figure class="image is-128x128">
-                  <img src="https://interbalance.org/wp-content/uploads/2021/08/flouffy-VBkIK3qj3QE-unsplash-scaled-e1631077364762.jpg">
-                </figure>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-              </div>
-            </article>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <p class="title">
+          <i class="fas fa-dog"></i> 강아지와 <i class="fas fa-cat"></i>고양이의
+          동행
+        </p>
+        <p class="subtitle">
+          강아지와 고양이에 대한 모든 정보를 볼 수 있는 곳 안녕하세요
+        </p>
+      </div>
+    </section>
+    <div class="columns">
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>강아지</p>
+            <button class="delete" aria-label="delete"></button>
           </div>
-          <div class="column">
-            <article class="message">
-              <div class="message-header">
-                <p>고양이</p>
-                <button class="delete" aria-label="delete"></button>
-              </div>
-              <div class="message-body">
-                <figure class="image is-128x128">
-                  <img src="~assets/cat.jpg">
-                </figure>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-              </div>
-            </article>
+          <div class="message-body">
+            <figure class="image is-128x128">
+              <img
+                src="https://interbalance.org/wp-content/uploads/2021/08/flouffy-VBkIK3qj3QE-unsplash-scaled-e1631077364762.jpg"
+              />
+            </figure>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
+            porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam
+            gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
+            Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales,
+            arcu et sollicitudin porttitor, tortor urna tempor ligula, id
+            porttitor mi magna a neque. Donec dui urna, vehicula et sem eget,
+            facilisis sodales sem.
           </div>
-        </div>
+        </article>
+        <article class="notification">
+          <table
+            class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+          >
+            <thead>
+              <tr>
+                <th>분류</th>
+                <th>이름</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>개</td>
+                <td>동물계</td>
+              </tr>
+              <tr class="is-selected">
+                <td>믹스</td>
+                <td>가온</td>
+              </tr>
+              <tr>
+                <td>푸들</td>
+                <td>초코</td>
+              </tr>
+            </tbody>
+          </table>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>고양이</p>
+            <button class="delete" aria-label="delete"></button>
+          </div>
+          <div class="message-body">
+            <figure class="image is-128x128">
+              <img src="~assets/cat.jpg" />
+            </figure>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
+            porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam
+            gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
+            Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales,
+            arcu et sollicitudin porttitor, tortor urna tempor ligula, id
+            porttitor mi magna a neque. Donec dui urna, vehicula et sem eget,
+            facilisis sodales sem.
+          </div>
+        </article>
+        <article class="notification">
+          <table
+            class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+          >
+            <thead>
+              <tr>
+                <th>분류</th>
+                <th>이름</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>개</td>
+                <td>동물계</td>
+              </tr>
+              <tr class="is-selected">
+                <td>믹스</td>
+                <td>가온</td>
+              </tr>
+              <tr>
+                <td>푸들</td>
+                <td>초코</td>
+              </tr>
+            </tbody>
+          </table>
+        </article>
+      </div>
     </div>
+  </div>
 </template>
